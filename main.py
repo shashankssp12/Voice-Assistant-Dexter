0e114fb4b3157f8041440f17331710319c690372
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-07-30 16:47:14 +0530

    deepgram api integration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-import webbrowser #
+import webbrowser 
 import pyttsx3
 import musicLibrary
 import time
@@ -7,12 +7,24 @@ import os
 import requests #library to fetch the api and access the methods in it
 from dotenv import load_dotenv
 from openai import OpenAI
+import asyncio
+
+from deepgram import (
+    DeepgramClient,
+    PrerecordedOptions,
+    FileSource,
+)
+import pyaudio
+import wave
 
 # Load environment variables from .env file
 load_dotenv() 
 
 news_api = os.environ.get("NEWS_API_KEY") 
 url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey={news_api}'
+
+DEEPGRAM_API_KEY = os.environ.get("DEEPGRAM_API_KEY")
+
 # --------------
 
 recognizer = sr.Recognizer() #recognizes the audio
@@ -94,36 +106,87 @@ def greetUser():
         speak("Good Evening Sir!")
     speak("The current time is " + time.strftime("%I:%M %p"))
     speak("How may I help you today?")
-
-def takeCommand():
-    with sr.Microphone() as source:
-        print("Listening...")
-        
-        # recognizer.pause_threshold = 1 #seconds of non-speaking audio before a phrase is considered complete 
     
-        audio = recognizer.listen(source , timeout=2 #
-                                  , phrase_time_limit=2 #
-                                  ) #listening to the source
-        try:
-            print("Recognizing...")
-            query = recognizer.recognize_google(audio, language='en-in')
-            print("You said: "+query)            
-        except Exception as e:
-            print("Say that again please...")
-            print(e)
-            
-        return query
+    
+    
+    
+    
+def record_audio(output_filename, record_seconds=5, sample_rate=44100, chunk_size=1024):
+    audio_format = pyaudio.paInt16  # 16-bit resolution
+    channels = 1  # Mono
+
+    audio = pyaudio.PyAudio()
+
+    # Start Recording
+    stream = audio.open(format=audio_format, channels=channels,
+                        rate=sample_rate, input=True,
+                        frames_per_buffer=chunk_size)
+    print("Recording...")
+    frames = []
+
+    for _ in range(0, int(sample_rate / chunk_size * record_seconds)):
+        data = stream.read(chunk_size)
+        frames.append(data)
+
+    print("Finished recording.")
+
+    # Stop Recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # Save the recorded data as a WAV file
+    with wave.open(output_filename, 'wb') as wf:
+        wf.setnchannels(channels)
+        wf.setsampwidth(audio.get_sample_size(audio_format))
+        wf.setframerate(sample_rate)
+        wf.writeframes(b''.join(frames))
+
+
+def take_command(): # also returns the text from the audio
+   
+    record_audio('command.wav')
+    AUDIO_FILE = "command.wav"
+    try:
+        # STEP 1 Create a Deepgram client using the API key
+        deepgram = DeepgramClient(DEEPGRAM_API_KEY)
+
+        with open(AUDIO_FILE, "rb") as file:
+            buffer_data = file.read()
+
+        payload: FileSource = {
+            "buffer": buffer_data,
+        }
+
+        #STEP 2: Configure Deepgram options for audio analysis
+        options = PrerecordedOptions(
+            model="nova-2",
+            smart_format=True,
+        )
+
+        # STEP 3: Call the transcribe_file method with the text payload and options
+        response = deepgram.listen.prerecorded.v("1").transcribe_file(payload, options)
+
+        # STEP 4: Print the response
+        return response.results.channels[0].alternatives[0].transcript
+
+    except Exception as e:
+        print(f"Exception: {e}")
+
 
 
 if __name__=='__main__': 
+    
+    # call_out = ["dexter", "desktop", "next", "extra", "dex", "desk"]
     speak("Initializing Dexter...")
     greetUser()
     while True:
         try:
-            trigger_word = takeCommand().lower()
-            if "dexter" or "desktop" or "next" or "extra" or "dex" or "desk" in trigger_word.lower(): #I havent added 'text' or 'start' or 'xender' as the trigger words , but still it is working and I am not able to understand why 
+            trigger_word = take_command()
+            print(trigger_word)
+            if "dexter" in trigger_word.lower():
                 speak("Sir")
-                command = takeCommand()
+                command = take_command()
                 if "exit" in command.lower():
                     speak("Goodbye Sir!")
                     break
@@ -131,6 +194,5 @@ if __name__=='__main__':
                     processCommand(command)
             else:
                 speak("I am not activated. Please say Dexter to activate me.")
-                
         except Exception as e:
-            print("Error while Parsing" , e)
+            print("Error while Parsing", e)
